baf817c8da6acb8707d2c12ec42aade0baab7790
andriSH30 <andrisetya93@gmail.com>
2020-05-08 02:35:12 +0800

    first

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,4 +9,18 @@ describe("HelloWorld.vue", () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
+
+  it('berhasil memanggil changeMessage melalui aksi klik', () => {
+    const msgExpected = 'Sebuah pesan'
+    const msgAfterChangeExpected = 'Sebuah pesan baru'
+    const wrapper = shallowMount(HelloWorld)
+    expect(wrapper.find('.message')).toBe(msgExpected)
+
+    // memanggil method changeMessage lewat aksi klik
+    const buttonElemen = wrapper.find('.btn')
+    buttonElemen.trigger('click')
+
+    // mengecek perubahan setelah klik
+    expect(wrapper.find('.message')).toBe(msgAfterChangeExpected)
+  })
 });
